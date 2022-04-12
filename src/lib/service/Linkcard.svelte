6b71b80cb8f9e01c895eafd29cf1e42d1ec2068f
Farhaan Khan <farhaankhanfiroz@gmail.com>
2022-04-12 19:31:26 +0530

    Make user uploads functional

--- src/lib/service/Linkcard.svelte
+++ src/lib/service/Linkcard.svelte
@@ -1,19 +1,27 @@
 <script>
+    let link = ""
 
+    const clearLink = () => {
+        link = ""
+    }
+
+    const submitLink = () => {
+        alert("submitted")
+    }
 </script>
 
 <div class="flex justify-center items-center bg-neutral-100 w-11/12 py-8 rounded-xl mt-6 xs:mt-8 xs:max-w-sm sm:mb-4 sm:max-w-md sm:py-10 md:mt-10 lg:max-w-lg lg:py-12 xl:max-w-xl">
     <div class="flex flex-col items-center flex-grow">
-        <div class="px-4 w-full max-w-xs md:max-w-sm xl:max-w-md">
-            <input class="border border-neutral-600 border-opacity-30 p-2 w-full rounded-md placeholder:text-sm focus:outline-accent xs:p-3 lg:placeholder:text-base" type="text" name="link" id="link" placeholder="Youtube video link">
-        </div>
-        <div class="grid grid-cols-1 px-4 mt-8 gap-3 xs:gap-4 md:grid-cols-2 w-full max-w-xs sm:grid-cols-2 md:max-w-sm xl:max-w-md xl:gap-10 xl:mt-9">
-            <div class="sm:order-2">
-                <a href="#Services" class="inline-block w-full text-center bg-accent text-neutral-100 uppercase text-xs font-medium py-3.5 px-8 rounded-md tracking-wider hover:shadow-lg hover:bg-accent-dark transition-colors xs:text-sm sm:py-3">confirm</a>
+        <form on:submit|preventDefault={submitLink} autocomplete="off" class="px-4 w-full max-w-xs md:max-w-sm xl:max-w-md">
+            <input bind:value={link} required class="border border-neutral-600 border-opacity-30 p-2 w-full rounded-md placeholder:text-sm focus:outline-accent xs:p-3 lg:placeholder:text-base" type="text" name="link" id="link" placeholder="Youtube video link">
+            <div class="grid grid-cols-1 mt-8 gap-3 xs:gap-4 md:grid-cols-2 w-full max-w-xs sm:grid-cols-2 md:max-w-sm xl:max-w-md xl:gap-10 xl:mt-9">
+                <div class="sm:order-2">
+                    <button type="submit" class="inline-block w-full text-center bg-accent text-neutral-100 uppercase text-xs font-medium py-3.5 px-8 rounded-md tracking-wider hover:shadow-lg hover:bg-accent-dark transition-colors xs:text-sm sm:py-3">confirm</button>
+                </div>
+                <div class="sm:order-1">
+                    <button on:click|preventDefault={clearLink} class="inline-block w-full text-center bg-neutral-100 text-accent border border-accent uppercase text-xs font-medium py-3 px-8 rounded-md tracking-wider hover:shadow-lg xs:text-sm sm:py-3">clear</button>
+                </div>
             </div>
-            <div class="sm:order-1">
-                <a href="#Features" class="inline-block w-full text-center bg-neutral-100 text-accent border border-accent uppercase text-xs font-medium py-3 px-8 rounded-md tracking-wider hover:shadow-lg xs:text-sm sm:py-3">clear</a>
-            </div>
-        </div>
+        </form>
     </div>
 </div>
--- src/lib/service/Upload.svelte
+++ src/lib/service/Upload.svelte
@@ -1,16 +1,38 @@
 <script>
+    let isDragged = false
+    let imageFile
+    const validExtensions = ['image/png','image/jpeg','image/jpg']
 
+    const toggleActive = () => {
+        isDragged = !isDragged
+    }
+
+    const handleDrop = (event) => {
+        imageFile = event.dataTransfer.files[0]
+        if(validExtensions.includes(imageFile.type)) {
+            console.log("Valid image")
+        } else {
+            alert("Invalid image")
+        }
+    }
+
+    const handleInput = () => {
+        if(imageFile[0]) {
+            console.log(imageFile[0])
+        }
+    }
 </script>
 
 <div class="flex justify-center items-center bg-neutral-100 w-11/12 h-80 mt-6 rounded-xl xs:max-w-sm xs:mt-8 sm:max-w-lg sm:mt-10 md:max-w-xl md:h-88 lg:max-w-2xl xl:max-w-3xl">
-    <div class="flex justify-center items-center m-2 w-11/12 h-72 rounded-xl border border-dashed border-neutral-600 border-opacity-40 md:h-80">
+    <div on:dragenter|preventDefault={toggleActive} on:dragleave|preventDefault={toggleActive} on:dragover|preventDefault on:drop|preventDefault={toggleActive} on:drop|preventDefault={handleDrop} class="flex justify-center items-center m-2 w-11/12 h-72 rounded-xl border border-dashed border-neutral-600 border-opacity-40 md:h-80 {isDragged ? 'bg-accent bg-opacity-10 border-opacity-100' : ''}">
         <div class="mx-3 flex flex-col items-center">
             <p class="text-center text-neutral-600 font-light text-sm lg:text-base">Drag & Drop your image here to upload</p>
             <p class="text-neutral-600 text-sm font-bold mt-3">OR</p>
             <div class="">
                 <label for="input" class="inline-block w-40 mt-3 text-center bg-neutral-100 text-accent border border-accent uppercase text-xs font-medium py-3 px-6 rounded-md tracking-wider cursor-pointer hover:shadow-lg">Browse Images</label>
-                <input class="hidden" type="file" name="image" id="input" accept="image/png, image/jpeg">
+                <input bind:files={imageFile} on:change={handleInput} class="hidden" type="file" name="image" id="input" accept="image/png, image/jpeg">
             </div>
+            <small class="text-xs mt-3 text-neutral-600">Supports: JPEG,JPG,PNG</small>
         </div>
     </div>
 </div>
